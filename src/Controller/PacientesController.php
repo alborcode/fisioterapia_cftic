d68007f7b6b689da8a8b434e62e2b14a5033ad90
Luis Alberto Rubio <alborcode@gmail.com>
2022-07-29 16:25:39 +0200

    instalacion inicial con React Bootstrap-React y Seguridad Symfony

--- src/Controller/PacientesController.php
+++ src/Controller/PacientesController.php
@@ -11,39 +11,38 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Pacientes;
 use Doctrine\ORM\EntityManagerInterface;
 
-/** * @Security("is_granted('ROLE_USER'), statusCode=404, message="Conectese en el sistema para ver recurso.") */ 
+///** * @Security("is_granted('ROLE_USER'), statusCode=404, message="Conectese en el sistema para ver recurso.") */
 class PacientesController extends AbstractController
 {
-
     // Formulario de Conexion
     #[Route('/loginpaciente', name: 'loginpaciente')]
     public function loginpaciente()
-    { 
+    {
         return $this->render('pacienteIni.html.twig');
     }
 
     // Registrar nuevo Paciente
     #[Route('/newpaciente', name: 'altaPaciente')]
     public function altaPaciente(Request $request, EntityManagerInterface $em)
-    { 
+    {
         // Recupero el idusuario del campo oculto del formulario de Pacientes
-        $objetousuario  = $request->request->get('objetoIdusuario');
-        
-        // Recupero los datos de paciente del formulario
-        $nombre     = $request->request->get('txtNombre');
-        $apellido1  = $request->request->get('txtApellido1');
-        $apellido2  = $request->request->get('txtApellido2');
-        $direccion  = $request->request->get('txtDireccion');
-        $poblacion  = $request->request->get('txtPoblacion');
-        $cp         = $request->request->get('txtCp');
-        $provincia  = $request->request->get('comboProvincia');
-        $telefono   = $request->request->get('txtTelefono');
+        $objetousuario = $request->request->get('objetoIdusuario');
 
-        // Nueva instancia de Paciente      
+        // Recupero los datos de paciente del formulario
+        $nombre = $request->request->get('txtNombre');
+        $apellido1 = $request->request->get('txtApellido1');
+        $apellido2 = $request->request->get('txtApellido2');
+        $direccion = $request->request->get('txtDireccion');
+        $poblacion = $request->request->get('txtPoblacion');
+        $cp = $request->request->get('txtCp');
+        $provincia = $request->request->get('comboProvincia');
+        $telefono = $request->request->get('txtTelefono');
+
+        // Nueva instancia de Paciente
         $paciente = new Pacientes();
-        
+
         //dump("$objetousuario".$objetousuario);
-        $paciente->setIdusuario($objetousuario); 
+        $paciente->setIdusuario($objetousuario);
 
         $paciente->setNombre($nombre);
         $paciente->setApellido1($apellido1);
@@ -53,16 +52,14 @@ class PacientesController extends AbstractController
         $paciente->setCp($cp);
         $paciente->setProvincia($provincia);
         $paciente->setTelefono($telefono);
-                
+
         $em->persist($paciente);
         $em->flush();
-      
+
         $mensaje = 'Paciente dado de Alta';
         // Redirigimos conexion a Pagina Inicial de Pacientes tras alta de Paciente
-        return $this->render('pacienteIni.html.twig', 
-        [
-            ['mensaje' => $mensaje]
+        return $this->render('pacienteIni.html.twig', [
+            ['mensaje' => $mensaje],
         ]);
-            
     }
 }
